c79156dabd594f3afdb533837de0c7f669819456
Tomas <tumabarbosa@gmail.com>
2024-02-17 19:04:44 +0000

    Bug fix tp2

--- TP2/script.py
+++ TP2/script.py
@@ -1,51 +1,50 @@
 import re
 
-def converter(text):
+
+def converter(htmlBody):
     # Cabeçalhos
     ## H1
-    text = re.sub(r'^#(\s.*)$', r'<h1>\1</h1>', text, flags=re.MULTILINE)
+    htmlBody = re.sub(r'^#(\s.*)$', r'<h1>\1</h1>', htmlBody, flags=re.MULTILINE)
     ## H2
-    text = re.sub(r'^##(\s.*)$', r'<h2>\1</h2>', text, flags=re.MULTILINE)
+    htmlBody = re.sub(r'^##(\s.*)$', r'<h2>\1</h2>', htmlBody, flags=re.MULTILINE)
     ## H3
-    text = re.sub(r'^###(\s.*)$', r'<h3>\1</h3>', text, flags=re.MULTILINE)
+    htmlBody = re.sub(r'^###(\s.*)$', r'<h3>\1</h3>', htmlBody, flags=re.MULTILINE)
 
     # BOLD
-    text = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', text)
+    htmlBody = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', htmlBody)
 
     # ITALICO
-    text = re.sub(r'\*([^\*]*)\*', r'<i>\1</i>', text)
+    htmlBody = re.sub(r'\*([^\*]*)\*', r'<i>\1</i>', htmlBody)
 
     # LISTA NUMERADA
-    text = re.sub(r'^\s*\d+\.\s+(.+)$', r'<li>\1</li>', text, flags=re.MULTILINE)
+    htmlBody = re.sub(r'^\s*\d+\.\s+(.+)$', r'<li>\1</li>', htmlBody, flags=re.MULTILINE)
 
     # IMAGEM
-    text = re.sub(r'!\[([^\]]*)\]\(([^\)]*)\)', r'<img src="\2" alt="\1"/>', text)
+    htmlBody = re.sub(r'!\[([^\]]*)\]\(([^\)]*)\)', r'<img src="\2" alt="\1"/>', htmlBody)
 
     # LINK
-    text = re.sub(r'\[([^\]]*)\]\(([^\)]*)\)', r'<a href="\2">\1</a>', text)
-
+    htmlBody = re.sub(r'\[([^\]]*)\]\(([^\)]*)\)', r'<a href="\2">\1</a>', htmlBody)
 
     isFirst = True
     hasStarted = False
     finalText = ""
     # Check list
-    for line in text.splitlines():
+    for line in htmlBody.splitlines():
         if line.startswith("<li>") and isFirst:
-            finalText += "<ol>\n" + line+"\n"
+            finalText += "<ol>\n" + line + "\n"
             isFirst = False
             hasStarted = True
         elif hasStarted and not line.startswith("<li>"):
-            finalText += "</ol>\n" + line+"\n"
+            finalText += "</ol>\n" + line + "\n"
             isFirst = True
             hasStarted = False
         else:
-            finalText += line+"\n"
+            finalText += line + "\n"
 
     return finalText
 
 
 def main():
-
     finalHTMLText = """
 <!DOCTYPE html>
 <html lang="pt PT">
@@ -53,19 +52,19 @@ def main():
     <title>Pagina</title>
     <meta charset="utf-8">
 </head>
-    
+
 <body>
-    
+
 """
 
     with open("inputFile.md") as file:
         finalHTMLText += converter(file.read())
 
-
     finalHTMLText += "\n\n</body>"
 
     with open("outputFile.html", "w") as file:
         file.write(finalHTMLText)
+
 
 if __name__ == "__main__":
     main()
